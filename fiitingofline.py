adedaa24d204a6f58c1112d89bf5c94bdf565393
NIHALSAYYAD15 <90887293+NIHALSAYYAD15@users.noreply.github.com>
2022-06-09 15:53:59 +0530

    Update fiitingofline.py

--- fiitingofline.py
+++ fiitingofline.py
@@ -22,3 +22,19 @@ x=np.array(X)
 y=np.array(Y)
 model1.fit(x,y, epochs=50000)
 print(model1.predict([10]))
+
+
+#
+
+#Q6 demonstration simple module learning program for y=2x-1 code
+
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+model = tf.keras.Sequential([keras.layers.Dense(units =1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+print(model.summary())
+xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+model.fit(xs, ys, epochs = 500)
+print(model.predict([10.0]))
